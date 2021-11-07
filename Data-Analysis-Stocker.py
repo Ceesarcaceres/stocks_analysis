2ab97d952ccac05833e3ff4144bd4e8e13576b14
Cesar <42512808+Ceesarcaceres@users.noreply.github.com>
2021-11-07 15:06:20 -0300

    Update Data-Analysis-Stocker.py

--- Data-Analysis-Stocker.py
+++ Data-Analysis-Stocker.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 #pip install stocker
-
-
-# In[25]:
-
 
 import numpy as np
 import pandas as pd
@@ -19,16 +9,9 @@ from stocker.predict import tomorrow
 import seaborn as sn
 
 
-# In[3]:
-
-
 Microsoft = 'MSFT'
 
-
-# #### Checando as diferenças usando diferentes períodos de data
-
-# In[6]:
-
+# Checando as diferenças usando diferentes períodos de data
 
 error1 = tomorrow(Microsoft, years = 1)[1]
 error2 = tomorrow(Microsoft, years = 2)[1]
@@ -38,12 +21,10 @@ print('Erro usando 2 anos de data:', error2, '%')
 print('Erro usando 3 anos de data:', error3, '%')
 
 
-# #### Aparentemente é melhor usar somente dados do último ano. Isso pode ser devido ao entendimento do modelo, se somente o comportamento mais recente esta envolvido no modelo, portanto é feita uma melhor predição.
+# Aparentemente é melhor usar somente dados do último ano. Isso pode ser devido ao entendimento do modelo, 
+# se somente o comportamento mais recente esta envolvido no modelo, portanto é feita uma melhor predição.
 
-# #### Agora vamos checar a diferença usando quantidades diferentes de dias para os passos de entradas:
-
-# In[7]:
-
+# Agora vamos checar a diferença usando quantidades diferentes de dias para os passos de entradas:
 
 error1 = tomorrow(Microsoft, steps = 1)[1]
 error2 = tomorrow(Microsoft, steps = 10)[1]
@@ -53,12 +34,10 @@ print('Erro usando 10 dias anterior de dados:', error2, '%')
 print('Erro usando 20 dias anterior de dados:', error3, '%')
 
 
-# #### Novamente, checando que a data mais recente é a melhor opção. Isso não significa que o modelo não está levando em conta todo o comportamento durante todo o período de tempo.
+# Novamente, checando que a data mais recente é a melhor opção. 
+# Isso não significa que o modelo não está levando em conta todo o comportamento durante todo o período de tempo.
 
 # #### Agora vamos checar a diferença usando diferentes features:
-
-# In[8]:
-
 
 error1 = tomorrow(Microsoft, features=['Open'])[1]
 error2 = tomorrow(Microsoft, features=['Low'])[1]
@@ -87,37 +66,21 @@ print('Error by including all the features:',error10,'%')
 print('Error by including the features from Yahoo Finance:',error11,'%')
 
 
-# #### Usando somente o preço de fechamento anterior mostra um erro mais aproximado do que usando todas as features, então é uma boa opção para economizar  tempo enquanto está executando o código. De qualquer forma, é recomendado implementar vários casos com diferentes features e escolher o caso com o menor erro.
+# Usando somente o preço de fechamento anterior mostra um erro mais aproximado do que usando todas as features, 
+# então é uma boa opção para economizar  tempo enquanto está executando o código. De qualquer forma, 
+# é recomendado implementar vários casos com diferentes features e escolher o caso com o menor erro.
 
-# #### Finalmente vamos checar o coeficiente de correlação de Pearson para cada feature contra os preços de fechamento
-
-# In[9]:
-
+# Finalmente vamos checar o coeficiente de correlação de Pearson para cada feature contra os preços de fechamento
 
 from stocker.get_data import correlation
-
-
-# In[27]:
 
 
 corr = correlation(Microsoft, interest = True, wiki_views = True, indicators = True)
 
 
-# In[30]:
-
-
 corr
-
-
-# In[11]:
-
 
 Microsoft_prediction = stocker.predict.tomorrow('MSFT')
 
-
-# In[13]:
-
-
 Microsoft_prediction
 # [Preço previsto, error(%), data da próx abertura]
-
